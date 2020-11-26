d68866bef4d4359df2f5088e4d04100b04bb80f7
Miro Phan <phanmir@gmail.com>
2020-11-26 22:48:19 +0100

    add ediff mods

--- init.el
+++ init.el
@@ -74,6 +74,18 @@
     (error (elpy-rgrep-symbol (thing-at-point 'symbol)))))
 (define-key elpy-mode-map (kbd "M-.") 'goto-def-or-rgrep)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ediff                                                                  ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'ediff)
+;; don't start another frame
+;; this is done by default in preluse
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+;; put windows side by side
+(setq ediff-split-window-function (quote split-window-horizontally))
+;;revert windows on exit - needs winner mode
+(winner-mode)
+(add-hook 'ediff-after-quit-hook-internal 'winner-undo)
 
 
 ;; User-defined init.el ends here
