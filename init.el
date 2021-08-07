c4624b2ba88de6346e5566ad6a726079e92678a9
Miro Phan <phanmir@gmail.com>
2021-08-07 17:19:03 +0200

    change query-replace shortcut for yabai

--- init.el
+++ init.el
@@ -65,6 +65,12 @@
 (global-auto-revert-mode t)         ;; Refresh buffer whenever file changed (for git checkout)
 (add-hook 'dired-mode-hook 'auto-revert-mode) ;; Refresh dired buffer when file changed
 
+;; ===================================
+;; Shortcuts
+;; ===================================
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "C-c r") 'query-replace)
+
 ;; Elpy customisation
 ;; Enhance elpy-goto-definition to run rgrep if failed
 (defun goto-def-or-rgrep ()
@@ -96,9 +102,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
- '(package-selected-packages (quote (elpy eply use-package))))
+   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
+ '(package-selected-packages '(elpy eply use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
